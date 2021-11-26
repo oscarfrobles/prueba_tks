9e0c6cb404e978a84b0a4f671e7e3d064c528deb
Oscar <oskijob@gmail.com>
2021-11-26 02:48:48 +0100

    add delete option

--- resources/views/planificaciones/index.blade.php
+++ resources/views/planificaciones/index.blade.php
@@ -25,7 +25,9 @@
                  <th scope="col"> Id </th>
                  <th scope="col"> Usuario asignado </th>
                  <th scope="col"> Fecha </th>
-                 <th scope="col"> Autoasignármela </th>
+                 <th scope="col"> Estado </th>
+                 <th scope="col"> Autoasignarla </th>
+                 <th scope="col">Eliminar</th>
               </tr>
            </thead>
            <tbody>
@@ -34,13 +36,24 @@
                  <td> {{ $item->id }} </td>
                  <td> {{ ($item->user_id != NULL) ? $item->user_id : 'No asignado' }}  </td>
                  <td> {{ (!is_null($item->dt_job)) ? $item->dt_job : 'No asignada' }} </td> 
+                 <td>  @if ($item->status == 2 ) 
+                        <span class="verde">Job Completo</span> 
+                        @elseif ($item->status == 1) 
+                        <span class="naranja">Asignado</span>
+                        @else
+                        <span class="azul">Por asignar</span>
+                        @endif 
+                  </td>
                  <td>
                      @if ($item->status == 0 ) 
                         <a href="/planificaciones/{{ $item->id }}/edit">Editar para {{ Auth::user()->name }}</a>
                      @else 
-                        ASIGNADO <a href="/planificaciones/{{ $item->id }}/edit">Ver</a>
+                        <a href="/planificaciones/{{ $item->id }}/edit">Ver</a>
                      @endif
                  </td>  
+                 <td>
+                  <a href="#">Eliminar</a>
+                 </td>
                </tr>                       
             @endforeach
            </tbody>
